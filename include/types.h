41966e0fad2d9e742ca9901c0bfd7d9a979f9072
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-07 13:45:30 -0600

    oops

--- include/types.h
+++ include/types.h
@@ -9,7 +9,7 @@ typedef unsigned int  uint32;
 typedef unsigned long uint64;
 
 //explicit-length signed ints
-typedef char uint8;
+typedef char int8;
 typedef short int16;
 typedef int  int32;
 typedef long int64;
