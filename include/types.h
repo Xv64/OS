6dc4a02f436fa7e80ec0333765163bc2d18efe7d
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-07 13:45:15 -0600

    just to get it out of the way...

--- include/types.h
+++ include/types.h
@@ -2,10 +2,17 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 
+//explicit-length unsigned ints
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int  uint32;
 typedef unsigned long uint64;
+
+//explicit-length signed ints
+typedef char uint8;
+typedef short int16;
+typedef int  int32;
+typedef long int64;
 
 #if X64
 typedef unsigned long uintp;
