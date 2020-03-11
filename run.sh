bbb70cba4902e1fd29cdda5974b52ffe2943056d
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-10 23:17:36 -0500

    add variables for legibility

--- run.sh
+++ run.sh
@@ -3,4 +3,8 @@
 # docker-compose build xv6
 # docker-compose run xv6
 
-qemu-system-x86_64 -net none -hda ./bin/boot.vdi -drive id=disk,file=./bin/fs.vdi,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -smp 3 -m 512 -nographic
+NETWORK="-net none"
+BOOT_DISK="-hda ./bin/boot.vdi"
+ROOT_DISK="-drive id=disk,file=./bin/fs.vdi,if=none -device ide-drive,drive=disk,bus=ahci.0"
+
+qemu-system-x86_64 -device ahci,id=ahci -smp 3 -m 512 -nographic $NETWORK $BOOT_DISK $ROOT_DISK
