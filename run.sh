961d1652833ef5a8088b098c548ef3d628404b61
Jason Whitehorn <jason@whitehorn.us>
2020-03-16 15:33:21 -0500

    update run.sh to match current expectations when using IDE drives

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ BOOT_DISK="-hda ./bin/boot.vdi"
 ROOT_DISK="-drive id=disk,file=./bin/fs.vdi,if=none -device ide-drive,drive=disk,bus=ahci.0"
 
 if [ -n "$IDE_MODE" ]; then
-    ROOT_DISK="-hdb ./bin/fs.vdi"
+    ROOT_DISK="-hdd ./bin/fs.vdi"
 fi
 
 qemu-system-x86_64 -device ahci,id=ahci -smp 3 -m 512 -nographic $NETWORK $BOOT_DISK $ROOT_DISK
