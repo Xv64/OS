27318eb402e51953eca29d2cf30c73c19864e33b
Jason Whitehorn <jason@whitehorn.us>
2020-03-24 07:39:13 -0500

    update to reflect my current setup - TODO: make this configurable

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "About to install Xv64 on your LOCAL machine."
-echo "This will DESTROY the contents of /dev/sdb and assumes a boot volume mounted at /mnt/xv64_boot."
+echo "This will DESTROY the contents of /dev/sdc and assumes a boot volume mounted at /mnt/xv64_boot."
 echo "If any of these assumptions are false you should STOP right now."
 
 read -p "Do you wish to continue? [y/N] " -n 1 -r
@@ -11,4 +11,4 @@ then
 fi
 echo ""
 cp ./bin/kernel /mnt/xv64_boot/boot/
-dd if=./bin/fs.img of=/dev/sdb
+dd if=./bin/fs.img of=/dev/sdc
