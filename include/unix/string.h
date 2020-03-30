95d5ab7fc445da400a8deb24d566a3c0ee169431
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-30 07:56:58 -0500

    uint becomes unsigned int

--- include/unix/string.h
+++ include/unix/string.h
@@ -4,14 +4,14 @@
 
 int   atoi(const char *s);
 char* gets(char *buf, int max);
-int   memcmp(const void* v1, const void* v2, uint n);
-void* memmove(void* dst, const void* src, uint n);
-void* memset(void* dst, int c, uint n);
+int   memcmp(const void* v1, const void* v2, unsigned int n);
+void* memmove(void* dst, const void* src, unsigned int n);
+void* memset(void* dst, int c, unsigned int n);
 char* safestrcpy(char* s, const char* t, int n);
 char* strcat_s(char *dest, char *right, int max_len);
 char* strchr(const char *s, char c);
 char* strcpy(char *s, char *t);
 int   strlen(const char* s);
-int   strncmp(const char* p, const char* q, uint n);
+int   strncmp(const char* p, const char* q, unsigned int n);
 char* strncpy(char* s, const char* t, int n);
 char* strstr(const char *s, char *s2);
