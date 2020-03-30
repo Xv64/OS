c1edba518cf7dc98a36a62927402cf619b8752a7
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-30 07:57:26 -0500

    define posix int types

---
+++ include/unix/stdint.h
@@ -0,0 +1,13 @@
+//poll.h - POSIX Base Definitions, Issue 6 - page 316
+
+
+typedef char int8_t;
+typedef short int16_t;
+typedef int  int32_t;
+typedef long int64_t;
+
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_T;
+typedef unsigned int  uint32_t;
+typedef unsigned long uint64_t;
