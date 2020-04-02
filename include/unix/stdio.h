2e914facea6adddd7d6b01147a22765c345083a0
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-01 23:43:09 -0500

    define stdin, stdout, & stderr

--- include/unix/stdio.h
+++ include/unix/stdio.h
@@ -1,6 +1,11 @@
 //poll.h - POSIX Base Definitions, Issue 6 - page 323
 #include "stddef.h"
 
+
+#define stdin  0
+#define stdout 1
+#define stderr 2
+
 //implemented in ulib/printf.c:
 int snprintf(char *s, size_t n, const char *fmt, ...);
 void fprintf(int fd, char *fmt, ...);
