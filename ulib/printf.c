3fc2365ba265b1a08afac7ddfd9fe26b671c0757
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-03 09:47:10 -0500

    WIP tracking printf length

--- ulib/printf.c
+++ ulib/printf.c
@@ -11,11 +11,12 @@
 #define PRINT_SCREEN 0
 #define PRINT_BUFFER 1
 
-static void putc(int fd, char c) {
+static int32 putc(int fd, char c) {
 	write(fd, &c, 1);
+    return 1;
 }
 
-static void printint(int fd, int xx, int base, int sgn) {
+static int32 printint(int fd, int xx, int base, int sgn) {
 	static char digits[] = "0123456789ABCDEF";
 	char buf[16];
 	int i, neg;
@@ -35,15 +36,21 @@ static void printint(int fd, int xx, int base, int sgn) {
 	} while((x /= base) != 0);
 	if(neg)
 		buf[i++] = '-';
+    int32 len = i;
 
 	while(--i >= 0)
 		putc(fd, buf[i]);
+
+    return len;
 }
 
 // This is the core print method, all external functions delegate to this.
 static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxlen), const char *fmt,  va_list ap){
 	char *s;
 	int c, i, state;
+    int32 len = 0; //based on mode this will either represent:
+                   //     when mode == PRINT_SCREEN: the total number of characters printed
+                   //     when mode == PRINT_BUFFER: the total number of characters that *could* have been written
 
     state = 0;
 	for(i = 0; fmt[i]; i++) {
@@ -53,7 +60,7 @@ static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxl
 				state = '%';
 			} else {
                 if(mode == PRINT_SCREEN){
-    				putc(fd, c);
+    				len += putc(fd, c);
                 }else{
                     //TODO
                 }
@@ -61,13 +68,13 @@ static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxl
 		} else if(state == '%') {
 			if(c == 'd') {
                 if(mode == PRINT_SCREEN){
-    				printint(fd, va_arg(ap, int), 10, 1);
+    				len += printint(fd, va_arg(ap, int), 10, 1);
                 }else{
                     //TODO
                 }
 			} else if(c == 'x' || c == 'p') {
                 if(mode == PRINT_SCREEN){
-    				printint(fd, va_arg(ap, int), 16, 0);
+    				len += printint(fd, va_arg(ap, int), 16, 0);
                 }else{
                     //TODO
                 }
@@ -77,7 +84,7 @@ static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxl
 					s = "(null)";
 				while(*s != 0) {
                     if(mode == PRINT_SCREEN){
-        				putc(fd, *s);
+        				len += putc(fd, *s);
                     }else{
                         //TODO
                     }
@@ -85,21 +92,21 @@ static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxl
 				}
 			} else if(c == 'c') {
                 if(mode == PRINT_SCREEN){
-                    putc(fd, va_arg(ap, uint));
+                    len += putc(fd, va_arg(ap, uint));
                 }else{
                     //TODO
                 }
 			} else if(c == '%') {
                 if(mode == PRINT_SCREEN){
-                    putc(fd, c);
+                    len += putc(fd, c);
                 }else{
                     //TODO
                 }
 			} else {
 				// Unknown % sequence.  Print it to draw attention.
                 if(mode == PRINT_SCREEN){
-                    putc(fd, '%');
-    				putc(fd, c);
+                    len += putc(fd, '%');
+    				len += putc(fd, c);
                 }else{
                     //TODO
                 }
@@ -108,7 +115,7 @@ static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxl
 		}
 	}
 
-    return 0; //TODO
+    return len;
 }
 
 void fprintf(int32 fd, const char *fmt, ...){
