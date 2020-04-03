6710937d6de2e0324bbd2cd32dfa326e571e6ad9
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-03 09:47:47 -0500

    formatting

--- ulib/printf.c
+++ ulib/printf.c
@@ -13,7 +13,7 @@
 
 static int32 putc(int fd, char c) {
 	write(fd, &c, 1);
-    return 1;
+	return 1;
 }
 
 static int32 printint(int fd, int xx, int base, int sgn) {
@@ -36,106 +36,106 @@ static int32 printint(int fd, int xx, int base, int sgn) {
 	} while((x /= base) != 0);
 	if(neg)
 		buf[i++] = '-';
-    int32 len = i;
+	int32 len = i;
 
 	while(--i >= 0)
 		putc(fd, buf[i]);
 
-    return len;
+	return len;
 }
 
 // This is the core print method, all external functions delegate to this.
 static int32 vprintf(uint8 mode, int32 fd, char *UNUSED(buf), uint32 UNUSED(maxlen), const char *fmt,  va_list ap){
 	char *s;
 	int c, i, state;
-    int32 len = 0; //based on mode this will either represent:
-                   //     when mode == PRINT_SCREEN: the total number of characters printed
-                   //     when mode == PRINT_BUFFER: the total number of characters that *could* have been written
+	int32 len = 0; //based on mode this will either represent:
+	               //     when mode == PRINT_SCREEN: the total number of characters printed
+	               //     when mode == PRINT_BUFFER: the total number of characters that *could* have been written
 
-    state = 0;
+	state = 0;
 	for(i = 0; fmt[i]; i++) {
 		c = fmt[i] & 0xff;
 		if(state == 0) {
 			if(c == '%') {
 				state = '%';
 			} else {
-                if(mode == PRINT_SCREEN){
-    				len += putc(fd, c);
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += putc(fd, c);
+				}else{
+					//TODO
+				}
 			}
 		} else if(state == '%') {
 			if(c == 'd') {
-                if(mode == PRINT_SCREEN){
-    				len += printint(fd, va_arg(ap, int), 10, 1);
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += printint(fd, va_arg(ap, int), 10, 1);
+				}else{
+					//TODO
+				}
 			} else if(c == 'x' || c == 'p') {
-                if(mode == PRINT_SCREEN){
-    				len += printint(fd, va_arg(ap, int), 16, 0);
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += printint(fd, va_arg(ap, int), 16, 0);
+				}else{
+					//TODO
+				}
 			} else if(c == 's') {
 				s = va_arg(ap, char*);
 				if(s == 0)
 					s = "(null)";
 				while(*s != 0) {
-                    if(mode == PRINT_SCREEN){
-        				len += putc(fd, *s);
-                    }else{
-                        //TODO
-                    }
+					if(mode == PRINT_SCREEN) {
+						len += putc(fd, *s);
+					}else{
+						//TODO
+					}
 					s++;
 				}
 			} else if(c == 'c') {
-                if(mode == PRINT_SCREEN){
-                    len += putc(fd, va_arg(ap, uint));
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += putc(fd, va_arg(ap, uint));
+				}else{
+					//TODO
+				}
 			} else if(c == '%') {
-                if(mode == PRINT_SCREEN){
-                    len += putc(fd, c);
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += putc(fd, c);
+				}else{
+					//TODO
+				}
 			} else {
 				// Unknown % sequence.  Print it to draw attention.
-                if(mode == PRINT_SCREEN){
-                    len += putc(fd, '%');
-    				len += putc(fd, c);
-                }else{
-                    //TODO
-                }
+				if(mode == PRINT_SCREEN) {
+					len += putc(fd, '%');
+					len += putc(fd, c);
+				}else{
+					//TODO
+				}
 			}
 			state = 0;
 		}
 	}
 
-    return len;
+	return len;
 }
 
 void fprintf(int32 fd, const char *fmt, ...){
-    va_list args;
-    va_start(args, fmt);
-    vprintf(PRINT_SCREEN, fd, 0, 0, fmt, args);
-    va_end(args);
+	va_list args;
+	va_start(args, fmt);
+	vprintf(PRINT_SCREEN, fd, 0, 0, fmt, args);
+	va_end(args);
 }
 
 void printf(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vprintf(PRINT_SCREEN, 0, 0, 0, fmt, args);
-    va_end(args);
+	va_list args;
+	va_start(args, fmt);
+	vprintf(PRINT_SCREEN, 0, 0, 0, fmt, args);
+	va_end(args);
 }
 
 int snprintf(char *s, unsigned int n, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    int result = vprintf(PRINT_BUFFER, 0, s, n, fmt, args);
-    va_end(args);
-    return result;
+	va_list args;
+	va_start(args, fmt);
+	int result = vprintf(PRINT_BUFFER, 0, s, n, fmt, args);
+	va_end(args);
+	return result;
 }
