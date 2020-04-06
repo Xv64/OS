4eb63e5aa2ffb4b3a02eadf7e4b2d6becb08da2c
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-05 23:55:34 -0500

    defien is undefined, lol

--- kernel/uart.c
+++ kernel/uart.c
@@ -14,7 +14,7 @@
 #define COM1      0x3F8
 #define COM2      0x2F8
 #define COM3      0x3E8
-#defien COM4      0x2E8
+#define COM4      0x2E8
 #define BAUD_RATE 115200
 
 static int uart;    // is there a uart?
