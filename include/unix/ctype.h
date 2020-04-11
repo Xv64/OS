b01501759eba8a24f3529a28aa19569d3844506f
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-10 21:50:45 -0500

    implement POSIX isspace

---
+++ include/unix/ctype.h
@@ -0,0 +1,3 @@
+//ctype.h - POSIX Base Definitions, Issue 6 - page 214
+
+int isspace(int c);
--- ulib/posix.c
+++ ulib/posix.c
@@ -173,3 +173,15 @@ char* strstr(const char *str1, char *str2) {
 
     return 0;
 }
+
+int isspace(int c) {
+    //"The isspace( ) function shall return non-zero if c is a white-space character;
+    //otherwise, it shall return 0."  - POSIX Base Spec, Issue 6 page 647
+    char chr = (char)c;
+    if(chr == ' ' || chr == '\t' || chr == '\v' || chr == '\n' || chr == '\f' || chr == '\r'){
+        //the above is the list of all ASCII-space whitespace character codes
+        //see: https://en.wikipedia.org/wiki/Whitespace_character#Unicode
+        return 1;
+    }
+    return 0;
+}
