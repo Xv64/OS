b07a8e9873b56cc5b8e9777c987132305ae948d1
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-12 00:12:19 -0500

    extract syscalls into their own header

---
+++ include/syscalls.h
@@ -0,0 +1,25 @@
+struct stat;
+
+// system calls
+int fork(void);
+int exit(void) __attribute__((noreturn));
+int wait(void);
+int pipe(int*);
+int write(int, void*, int);
+int read(int, void*, int);
+int close(int);
+int kill(int);
+int exec(char*, char**);
+int open(char*, int);
+int mknod(char*, short, short);
+int unlink(char*);
+int fstat(int fd, struct stat*);
+int link(char*, char*);
+int mkdir(char*);
+int chdir(char*);
+int dup(int);
+int getpid(void);
+char* sbrk(int);
+int sleep(int);
+int uptime(void);
+void reboot(void);
--- include/user.h
+++ include/user.h
@@ -1,28 +1,4 @@
-struct stat;
-
-// system calls
-int fork(void);
-int exit(void) __attribute__((noreturn));
-int wait(void);
-int pipe(int*);
-int write(int, void*, int);
-int read(int, void*, int);
-int close(int);
-int kill(int);
-int exec(char*, char**);
-int open(char*, int);
-int mknod(char*, short, short);
-int unlink(char*);
-int fstat(int fd, struct stat*);
-int link(char*, char*);
-int mkdir(char*);
-int chdir(char*);
-int dup(int);
-int getpid(void);
-char* sbrk(int);
-int sleep(int);
-int uptime(void);
-void reboot(void);
+#include "syscalls.h"
 
 // ulib.c
 #define TIOCGWINSZ 0x100
