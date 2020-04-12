aa61765904c92e5cc0c4b7fc29c26a0a8f95c8ad
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-12 18:01:29 -0500

    proper casing is important

--- include/unix/stdint.h
+++ include/unix/stdint.h
@@ -8,6 +8,6 @@ typedef long int64_t;
 
 
 typedef unsigned char uint8_t;
-typedef unsigned short uint16_T;
+typedef unsigned short uint16_t;
 typedef unsigned int  uint32_t;
 typedef unsigned long uint64_t;
