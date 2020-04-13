61bbf2e2b752eeb4315183dc228e4564cbfaea97
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-12 21:03:27 -0500

    use more generic C in this header as it will be shared various places

--- include/x86.h
+++ include/x86.h
@@ -1,15 +1,14 @@
 // Routines to let C code use special amd64 instructions.
 // See: http://ref.x86asm.net/coder64.html
 
-static inline uchar inb(ushort port){
-	uchar data;
+static inline unsigned char inb(unsigned short port){
+	unsigned char data;
 
 	asm volatile ("in %1,%0" : "=a" (data) : "d" (port));
 	return data;
 }
 
-static inline void
-amd64_nop(){
+static inline void amd64_nop(){
 	asm volatile ("nop");
 }
 
@@ -18,17 +17,16 @@ amd64_nop(){
 
 #define amd64_mem_barrier() asm volatile ("lock; addl $0,0(%%rsp)" : : : "memory")
 
-#define amd64_mem_read32(addr) (*(volatile uint32 *)(addr))
-#define amd64_mem_read64(addr) (*(volatile uint64 *)(addr))
+#define amd64_mem_read32(addr) (*(volatile unsigned int *)(addr))
+#define amd64_mem_read64(addr) (*(volatile unsigned long *)(addr))
 
-static volatile inline uint32
-amd64_spinread32(uint64 baseAddr, uint32 offset){
-	int16 total = 0;
-	int16 same = 0;
-	uint32 val;
+static volatile inline unsigned int amd64_spinread32(unsigned long baseAddr, unsigned int offset){
+	short total = 0;
+	short same = 0;
+	unsigned int val;
 	while(total < 1000 && same < 100) {
 		amd64_mem_barrier();
-		uint32 newVal = amd64_mem_read32(baseAddr + offset);
+		unsigned int newVal = amd64_mem_read32(baseAddr + offset);
 		if(val == newVal) {
 			same++;
 		}else{
@@ -40,14 +38,13 @@ amd64_spinread32(uint64 baseAddr, uint32 offset){
 	return val;
 }
 
-static volatile inline uint64
-amd64_spinread64(volatile uint64 *baseAddr, uint32 offset){
-	int16 total = 0;
-	int16 same = 0;
-	uint64 val;
+static volatile inline unsigned long amd64_spinread64(volatile unsigned long *baseAddr, unsigned int offset){
+	short total = 0;
+	short same = 0;
+	unsigned long val;
 	while(total < 1000 && same < 100) {
 		amd64_mem_barrier();
-		uint64 newVal = amd64_mem_read32(baseAddr + offset);
+		unsigned long newVal = amd64_mem_read32(baseAddr + offset);
 		if(val == newVal) {
 			same++;
 		}else{
@@ -69,34 +66,34 @@ static inline void insl(int port, void *addr, int cnt){
 	              "memory", "cc");
 }
 
-static inline void amd64_out8(uint16 port, uint8 data) {
+static inline void amd64_out8(unsigned short port, unsigned char data) {
 	asm volatile ("out %0,%1" : : "a" (data), "d" (port));
 }
 
-static inline uint16 amd64_in16(uint16 port){
-	uint16 result;
+static inline unsigned short amd64_in16(unsigned short port){
+	unsigned short result;
 	asm volatile ( "inw %1, %0"
 	               : "=a" (result) : "Nd" (port) );
 	return result;
 }
 
-static inline void amd64_out16(uint16 port, uint16 data) {
+static inline void amd64_out16(unsigned short port, unsigned short data) {
 	asm volatile ("out %0,%1" : : "a" (data), "d" (port));
 }
 
-static inline uint32 amd64_in32(uint16 port){
-	uint32 result;
+static inline unsigned int amd64_in32(unsigned short port){
+	unsigned int result;
 	asm volatile ( "inl %1, %0"
 	               : "=a" (result) : "Nd" (port) );
 	return result;
 }
 
-static inline void amd64_out32(uint16 port, uint32 data){
+static inline void amd64_out32(unsigned short port, unsigned int data){
 	asm volatile ("outl %0, %1" : : "a" (data), "Nd" (port));
 }
 
-static inline uint8 amd64_in8(uint16 port){
-	uint8 result;
+static inline unsigned char amd64_in8(unsigned short port){
+	unsigned char result;
 	asm volatile ( "inb %1, %0"
 	               : "=a" (result) : "Nd" (port) );
 	return result;
@@ -127,14 +124,14 @@ static inline void stosl(void *addr, int data, int cnt) {
 struct segdesc;
 
 static inline void lgdt(struct segdesc *p, int size) {
-	volatile ushort pd[5];
+	volatile unsigned short pd[5];
 
 	pd[0] = size-1;
-	pd[1] = (uintp)p;
-	pd[2] = (uintp)p >> 16;
+	pd[1] = (unsigned long)p;
+	pd[2] = (unsigned long)p >> 16;
 #if X64
-	pd[3] = (uintp)p >> 32;
-	pd[4] = (uintp)p >> 48;
+	pd[3] = (unsigned long)p >> 32;
+	pd[4] = (unsigned long)p >> 48;
 #endif
 	asm volatile ("lgdt (%0)" : : "r" (pd));
 }
@@ -142,29 +139,29 @@ static inline void lgdt(struct segdesc *p, int size) {
 struct gatedesc;
 
 static inline void lidt(struct gatedesc *p, int size) {
-	volatile ushort pd[5];
+	volatile unsigned short pd[5];
 
 	pd[0] = size-1;
-	pd[1] = (uintp)p;
-	pd[2] = (uintp)p >> 16;
+	pd[1] = (unsigned long)p;
+	pd[2] = (unsigned long)p >> 16;
 #if X64
-	pd[3] = (uintp)p >> 32;
-	pd[4] = (uintp)p >> 48;
+	pd[3] = (unsigned long)p >> 32;
+	pd[4] = (unsigned long)p >> 48;
 #endif
 	asm volatile ("lidt (%0)" : : "r" (pd));
 }
 
-static inline void ltr(ushort sel) {
+static inline void ltr(unsigned short sel) {
 	asm volatile ("ltr %0" : : "r" (sel));
 }
 
-static inline uintp readeflags(void) {
-	uintp eflags;
+static inline unsigned long readeflags(void) {
+	unsigned long eflags;
 	asm volatile ("pushf; pop %0" : "=r" (eflags));
 	return eflags;
 }
 
-static inline void loadgs(ushort v) {
+static inline void loadgs(unsigned short v) {
 	asm volatile ("movw %0, %%gs" : : "r" (v));
 }
 
@@ -180,8 +177,8 @@ static inline void hlt(void) {
 	asm volatile ("hlt");
 }
 
-static inline uint xchg(volatile uint *addr, uintp newval) {
-	uint result;
+static inline unsigned int xchg(volatile unsigned int *addr, unsigned long newval) {
+	unsigned int result;
 
 	// The + in "+m" denotes a read-modify-write operand.
 	asm volatile ("lock; xchgl %0, %1" :
@@ -191,17 +188,17 @@ static inline uint xchg(volatile uint *addr, uintp newval) {
 	return result;
 }
 
-static inline uintp rcr2(void) {
-	uintp val;
+static inline unsigned long rcr2(void) {
+	unsigned long val;
 	asm volatile ("mov %%cr2,%0" : "=r" (val));
 	return val;
 }
 
-static inline void lcr3(uintp val) {
+static inline void lcr3(unsigned long val) {
 	asm volatile ("mov %0,%%cr3" : : "r" (val));
 }
 
-static inline void amd64_cpuid(uint ax, uint32 *p) {
+static inline void amd64_cpuid(unsigned int ax, unsigned int *p) {
 	asm volatile ("cpuid"
 	              : "=a" (p[0]), "=b" (p[1]), "=c" (p[2]), "=d" (p[3])
 	              :  "0" (ax));
@@ -214,64 +211,64 @@ static inline void amd64_cpuid(uint ax, uint32 *p) {
 // lie about some register names in 64bit mode to avoid
 // clunky ifdefs in proc.c and trap.c.
 struct trapframe {
-	uint64 eax; // rax
-	uint64 rbx;
-	uint64 rcx;
-	uint64 rdx;
-	uint64 rbp;
-	uint64 rsi;
-	uint64 rdi;
-	uint64 r8;
-	uint64 r9;
-	uint64 r10;
-	uint64 r11;
-	uint64 r12;
-	uint64 r13;
-	uint64 r14;
-	uint64 r15;
+	unsigned long eax; // rax
+	unsigned long rbx;
+	unsigned long rcx;
+	unsigned long rdx;
+	unsigned long rbp;
+	unsigned long rsi;
+	unsigned long rdi;
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+	unsigned long r12;
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
 
-	uint64 trapno;
-	uint64 err;
+	unsigned long trapno;
+	unsigned long err;
 
-	uint64 eip; // rip
-	uint64 cs;
-	uint64 eflags; // rflags
-	uint64 esp; // rsp
-	uint64 ds; // ss
+	unsigned long eip; // rip
+	unsigned long cs;
+	unsigned long eflags; // rflags
+	unsigned long esp; // rsp
+	unsigned long ds; // ss
 };
 #else
 struct trapframe {
 	// registers as pushed by pusha
-	uint edi;
-	uint esi;
-	uint ebp;
-	uint oesp; // useless & ignored
-	uint ebx;
-	uint edx;
-	uint ecx;
-	uint eax;
+	unsigned int edi;
+	unsigned int esi;
+	unsigned int ebp;
+	unsigned int oesp; // useless & ignored
+	unsigned int ebx;
+	unsigned int edx;
+	unsigned int ecx;
+	unsigned int eax;
 
 	// rest of trap frame
-	ushort gs;
-	ushort padding1;
-	ushort fs;
-	ushort padding2;
-	ushort es;
-	ushort padding3;
-	ushort ds;
-	ushort padding4;
-	uint trapno;
+	unsigned short gs;
+	unsigned short padding1;
+	unsigned short fs;
+	unsigned short padding2;
+	unsigned short es;
+	unsigned short padding3;
+	unsigned short ds;
+	unsigned short padding4;
+	unsigned int trapno;
 
 	// below here defined by x86 hardware
-	uint err;
-	uint eip;
-	ushort cs;
-	ushort padding5;
-	uint eflags;
+	unsigned int err;
+	unsigned int eip;
+	unsigned short cs;
+	unsigned short padding5;
+	unsigned int eflags;
 
 	// below here only when crossing rings, such as from user to kernel
-	uint esp;
-	ushort ss;
-	ushort padding6;
+	unsigned int esp;
+	unsigned short ss;
+	unsigned short padding6;
 };
 #endif
