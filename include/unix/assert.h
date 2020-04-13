9a54d3646d190fd8f76c12bb9d60916d6255ef3b
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-12 23:06:45 -0500

    define & implement the POSIX assert() macro

---
+++ include/unix/assert.h
@@ -0,0 +1,15 @@
+//assert.h - POSIX Base Definitions, Issue 6 - page 209
+
+#ifdef NDEBUG
+
+#define assert(ignore)((void) 0)
+
+#else
+
+#define _str(x) #x
+#define _tostr(x) _str(x)
+#define _assert_occurs " [" __FILE__ ":" _tostr(__LINE__) "] "
+#define assert(x) \
+        do { if (!(x)) { fprintf(2, "assertion failed" _assert_occurs #x); abort(); } } while (0)
+
+#endif
