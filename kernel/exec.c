2d9fc88e29e017704aead4bcd35dcb0b13bae95c
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-04-29 16:16:35 -0500

    formatting & remove un-needed ifdef

--- kernel/exec.c
+++ kernel/exec.c
@@ -8,106 +8,102 @@
 #include "elf.h"
 #include "kernel/string.h"
 
-int
-exec(char *path, char **argv)
-{
-  char *s, *last;
-  int i, off;
-  uintp argc, sz, sp, ustack[3+MAXARG+1];
-  struct elfhdr elf;
-  struct inode *ip;
-  struct proghdr ph;
-  pde_t *pgdir, *oldpgdir;
+int exec(char *path, char **argv) {
+	char *s, *last;
+	int i, off;
+	uintp argc, sz, sp, ustack[3+MAXARG+1];
+	struct elfhdr elf;
+	struct inode *ip;
+	struct proghdr ph;
+	pde_t *pgdir, *oldpgdir;
 
-  begin_op();
-  if((ip = namei(path)) == 0){
-    end_op();
-    return -1;
-  }
-  ilock(ip);
-  pgdir = 0;
+	begin_op();
+	if((ip = namei(path)) == 0) {
+		end_op();
+		return -1;
+	}
+	ilock(ip);
+	pgdir = 0;
 
-  // Check ELF header
-  if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf))
-    goto bad;
-  if(elf.magic != ELF_MAGIC)
-    goto bad;
+	// Check ELF header
+	if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf))
+		goto bad;
+	if(elf.magic != ELF_MAGIC)
+		goto bad;
 
-  if((pgdir = setupkvm()) == 0)
-    goto bad;
+	if((pgdir = setupkvm()) == 0)
+		goto bad;
 
-  // Load program into memory.
-  sz = 0;
-  for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
-    if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
-      goto bad;
-    if(ph.type != ELF_PROG_LOAD)
-      continue;
-    if(ph.memsz < ph.filesz)
-      goto bad;
-    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
-      goto bad;
-    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
-      goto bad;
-  }
-  iunlockput(ip);
-  end_op();
-  ip = 0;
+	// Load program into memory.
+	sz = 0;
+	for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)) {
+		if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
+			goto bad;
+		if(ph.type != ELF_PROG_LOAD)
+			continue;
+		if(ph.memsz < ph.filesz)
+			goto bad;
+		if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
+			goto bad;
+		if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
+			goto bad;
+	}
+	iunlockput(ip);
+	end_op();
+	ip = 0;
 
-  // Allocate two pages at the next page boundary.
-  // Make the first inaccessible.  Use the second as the user stack.
-  sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
-    goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+	// Allocate two pages at the next page boundary.
+	// Make the first inaccessible.  Use the second as the user stack.
+	sz = PGROUNDUP(sz);
+	if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+		goto bad;
+	clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+	sp = sz;
 
-  // Push argument strings, prepare rest of stack in ustack.
-  for(argc = 0; argv[argc]; argc++) {
-    if(argc >= MAXARG)
-      goto bad;
-    sp = (sp - (strlen(argv[argc]) + 1)) & ~(sizeof(uintp)-1);
-    if(copyout(pgdir, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
-      goto bad;
-    ustack[3+argc] = sp;
-  }
-  ustack[3+argc] = 0;
+	// Push argument strings, prepare rest of stack in ustack.
+	for(argc = 0; argv[argc]; argc++) {
+		if(argc >= MAXARG)
+			goto bad;
+		sp = (sp - (strlen(argv[argc]) + 1)) & ~(sizeof(uintp)-1);
+		if(copyout(pgdir, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
+			goto bad;
+		ustack[3+argc] = sp;
+	}
+	ustack[3+argc] = 0;
 
-  ustack[0] = 0xffffffff;  // fake return PC
-  ustack[1] = argc;
-  ustack[2] = sp - (argc+1)*sizeof(uintp);  // argv pointer
+	ustack[0] = 0xffffffff; // fake return PC
+	ustack[1] = argc;
+	ustack[2] = sp - (argc+1)*sizeof(uintp); // argv pointer
 
-#if X64
-  proc->tf->rdi = argc;
-  proc->tf->rsi = sp - (argc+1)*sizeof(uintp);
-#endif
+	proc->tf->rdi = argc;
+	proc->tf->rsi = sp - (argc+1)*sizeof(uintp);
 
-  sp -= (3+argc+1) * sizeof(uintp);
-  if(copyout(pgdir, sp, ustack, (3+argc+1)*sizeof(uintp)) < 0)
-    goto bad;
+	sp -= (3+argc+1) * sizeof(uintp);
+	if(copyout(pgdir, sp, ustack, (3+argc+1)*sizeof(uintp)) < 0)
+		goto bad;
 
-  // Save program name for debugging.
-  for(last=s=path; *s; s++)
-    if(*s == '/')
-      last = s+1;
-  safestrcpy(proc->name, last, sizeof(proc->name));
+	// Save program name for debugging.
+	for(last=s=path; *s; s++)
+		if(*s == '/')
+			last = s+1;
+	safestrcpy(proc->name, last, sizeof(proc->name));
 
-  // Commit to the user image.
-  oldpgdir = proc->pgdir;
-  proc->pgdir = pgdir;
-  proc->sz = sz;
-  proc->tf->eip = elf.entry;  // main
-  proc->tf->esp = sp;
-  switchuvm(proc);
-  freevm(oldpgdir);
-  return 0;
+	// Commit to the user image.
+	oldpgdir = proc->pgdir;
+	proc->pgdir = pgdir;
+	proc->sz = sz;
+	proc->tf->eip = elf.entry; // main
+	proc->tf->esp = sp;
+	switchuvm(proc);
+	freevm(oldpgdir);
+	return 0;
 
- bad:
-  if(pgdir)
-    freevm(pgdir);
-  if(ip){
-    iunlockput(ip);
-    end_op();
-  }
-  return -1;
+bad:
+	if(pgdir)
+		freevm(pgdir);
+	if(ip) {
+		iunlockput(ip);
+		end_op();
+	}
+	return -1;
 }
