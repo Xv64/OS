42e5680140056738f1c6f77ab5caf8bee8a48ef0
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-05-17 23:56:19 -0500

    implement POSIX isdigit

--- include/unix/ctype.h
+++ include/unix/ctype.h
@@ -1,3 +1,4 @@
 //ctype.h - POSIX Base Definitions, Issue 6 - page 214
 
 int isspace(int c);
+int isdigit(int c);
--- ulib/unix/ctype.c
+++ ulib/unix/ctype.c
@@ -10,3 +10,14 @@ int isspace(int c) {
     }
     return 0;
 }
+
+
+int isdigit(int c) {
+    //The isdigit ( ) function shall return non-zero if c is a decimal digit; otherwise, it shall return 0.
+    //- POSIX Base Spec, Issue 6 page 632
+    if(c >= 48 && c <= 57){
+        //ASCII 48 -> 57 (inclusive) are digits 0-9
+        return 1;
+    }
+    return 0;
+}
