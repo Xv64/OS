22ee7eb470a194fa24ce79d26365538c11caacae
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-06-03 08:26:59 -0500

    fix include & strchr call in index

--- ulib/unix/strings.c
+++ ulib/unix/strings.c
@@ -1,6 +1,6 @@
-#include "string.h"
+#include "unix/string.h"
 
 char *index(const char *s, int c) {
     //"The index( ) function shall be equivalent to strchr( )." - POSIX Base Definitions, Issue 6
-    return strchar(s, c);
+    return strchr(s, c);
 }
