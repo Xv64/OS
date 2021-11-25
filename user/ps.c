2491adb9abbdf19ad395ffefddfa0529dd26b4d1
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-11-24 23:50:49 -0600

    list process names in ps

--- user/ps.c
+++ user/ps.c
@@ -21,13 +21,15 @@ char *statename(enum procstate state) {
 }
 
 int main(int argc, char *argv[]) {
+    char *name = malloc(32 * sizeof(char));
 
-    fprintf(stdout, "PID\tstate\n");
+    fprintf(stdout, "PID\tNAME\tSTATE\n");
     for(uint64 pid = 0; pid != NPROC; pid++) {
         enum procstate state = pstate(pid);
         if (state != UNUSED) {
             char *statestr = statename(state);
-            fprintf(stdout, "%d\t%s (%d)\n", pid, statestr,state);
+            pname(pid, name, 32);
+            fprintf(stdout, "%d\t%s\t%s (%d)\n", pid, name, statestr, state);
         }
     }
     procexit();
