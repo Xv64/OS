1454a103fdc8429de843cb997f3c7313c42677ed
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-11-27 17:25:37 -0600

    add debugging options

--- run.sh
+++ run.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-unset REBUILD IDE_MODE
+unset REBUILD IDE_MODE DEBUG
+CORES=3
 
-while getopts 'rl' c
+while getopts 'rld' c
 do
   case $c in
     r) REBUILD=TRUE ;;
     l) IDE_MODE=TRUE ;;
+    d) DEBUG=TRUE ;;
   esac
 done
 
 if [ -n "$REBUILD" ]; then
     ./build.sh || exit 1
+fi
+
+DEBUG_OPTS=""
+if [ -n "$DEBUG" ]; then
+    CORES=1
+    DEBUG_OPTS="-S"
+    echo "waiting on GDB..."
 fi
 
 NETWORK="-net none"
@@ -22,4 +31,4 @@ if [ -n "$IDE_MODE" ]; then
     ROOT_DISK="-hdd ./bin/fs.vdi"
 fi
 
-qemu-system-x86_64 $QEMU_OPTS -device ahci,id=ahci -smp 3 -m 512 $NETWORK $BOOT_DISK $ROOT_DISK
+qemu-system-x86_64 $QEMU_OPTS $DEBUG_OPTS -device ahci,id=ahci -smp $CORES -m 512 $NETWORK $BOOT_DISK $ROOT_DISK
