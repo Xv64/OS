dcbfcb83fcaf2c1f442fdd00e868d6b880f1b1d4
Jason Whitehorn <jason@whitehorn.us>
2021-12-01 09:04:04 -0600

    define max values for all uints

--- include/types.h
+++ include/types.h
@@ -18,3 +18,8 @@ typedef long int64;
 typedef unsigned long uintp;
 
 typedef uintp pde_t;
+
+#define UINT64_MAX 0xFFFFFFFFFFFFFFFF
+#define UINT32_MAX 0xFFFFFFFF
+#define UINT16_MAX 0xFFFF
+#define UINT8_MAX  0xFF
