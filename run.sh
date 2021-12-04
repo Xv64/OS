9b0a31833da5e269c10c3339b0520d9573292d88
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-12-04 12:30:59 -0600

    add run-time flag to toggle on/off EXT2

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-unset REBUILD IDE_MODE DEBUG
+unset REBUILD IDE_MODE DEBUG EXT2
 CORES=3
 
-while getopts 'rld' c
+while getopts 'rlde' c
 do
   case $c in
     r) REBUILD=TRUE ;;
     l) IDE_MODE=TRUE ;;
     d) DEBUG=TRUE ;;
+    e) EXT2=TRUE ;;
   esac
 done
 
@@ -23,12 +24,17 @@ if [ -n "$DEBUG" ]; then
     echo "waiting on GDB..."
 fi
 
+ROOT_IMG="fs.vdi"
+if [ -n "$EXT2" ]; then
+    ROOT_IMG="fs-ext2.img"
+fi
+
 NETWORK="-net none"
 BOOT_DISK="-hda ./bin/boot.vdi"
-ROOT_DISK="-drive id=disk,file=./bin/fs.vdi,if=none -device driver=ide-hd,drive=disk,bus=ahci.0"
+ROOT_DISK="-drive id=disk,file=./bin/$ROOT_IMG,if=none -device driver=ide-hd,drive=disk,bus=ahci.0"
 
 if [ -n "$IDE_MODE" ]; then
-    ROOT_DISK="-hdd ./bin/fs.vdi"
+    ROOT_DISK="-hdd ./bin/$ROOT_IMG"
 fi
 
 qemu-system-x86_64 $QEMU_OPTS $DEBUG_OPTS -device ahci,id=ahci -smp $CORES -m 512 $NETWORK $BOOT_DISK $ROOT_DISK
