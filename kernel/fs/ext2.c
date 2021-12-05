8281107f6488624bfec50ae0957df13264071a55
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-12-05 14:34:50 -0600

    reactor ext2_init_dev to make use of ext2_readsb

--- kernel/fs/ext2.c
+++ kernel/fs/ext2.c
@@ -8,23 +8,13 @@
 struct ext2_superblock sb;
 
 uint8 ext2_init_dev(uint16 devt, uint32 devnum) {
-    // assumes 512 byte sectors...
-
-    struct buf* bp1 = bread(TODEVNUM(devt, devnum), 2);
-    struct buf* bp2 = bread(TODEVNUM(devt, devnum), 3);
-
-    memcopy(&sb, &bp1->data[0], 512);
-    void *offset = &sb;
-    offset += 512;
-    memcopy(offset, &bp2->data[0], 512);
-
-    brelse(bp1);
-    brelse(bp2);
+    ext2_readsb(devt, devnum, &sb);
 
     return (sb.ext2_signature == FS_EXT2_SIGNATURE) ? 1 : 0;
 }
 
 void ext2_readsb(uint16 devt, uint32 devnum, struct ext2_superblock* sb2) {
+    // assumes 512 byte sectors...
     struct buf* bp1 = bread(TODEVNUM(devt, devnum), 2);
     struct buf* bp2 = bread(TODEVNUM(devt, devnum), 3);
 
