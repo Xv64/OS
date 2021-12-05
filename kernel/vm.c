7b2418d8d5ddb7f23e8a4dc5b7211a16f4c187b2
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-12-04 23:52:25 -0600

    remove dead code

--- kernel/vm.c
+++ kernel/vm.c
@@ -12,34 +12,6 @@ extern char data[];  // defined by kernel.ld
 pde_t* kpgdir;  // for use in scheduler()
 struct segdesc gdt[NSEGS];
 
-#ifndef X64
-// Set up CPU's kernel segment descriptors.
-// Run once on entry on each CPU.
-void seginit(void){
-	struct cpu* c;
-
-	// Map "logical" addresses to virtual addresses using identity map.
-	// Cannot share a CODE descriptor for both kernel and user
-	// because it would have to have DPL_USR, but the CPU forbids
-	// an interrupt from CPL=0 to DPL=3.
-	c = &cpus[cpunum()];
-	c->gdt[SEG_KCODE] = SEG(STA_X | STA_R, 0, 0xffffffff, 0);
-	c->gdt[SEG_KDATA] = SEG(STA_W, 0, 0xffffffff, 0);
-	c->gdt[SEG_UCODE] = SEG(STA_X | STA_R, 0, 0xffffffff, DPL_USER);
-	c->gdt[SEG_UDATA] = SEG(STA_W, 0, 0xffffffff, DPL_USER);
-
-	// Map cpu, and curproc
-	c->gdt[SEG_KCPU] = SEG(STA_W, &c->cpu, 8, 0);
-
-	lgdt(c->gdt, sizeof(c->gdt));
-	loadgs(SEG_KCPU << 3);
-
-	// Initialize cpu-local storage.
-	cpu = c;
-	proc = 0;
-}
-
-#endif
 
 // Return the address of the PTE in page table pgdir
 // that corresponds to virtual address va.  If alloc!=0,
@@ -86,88 +58,6 @@ static int mappages(pde_t* pgdir, void* va, uintp size, uintp pa, int perm){
 	}
 	return 0;
 }
-
-#ifndef X64
-// There is one page table per process, plus one that's used when
-// a CPU is not running any process (kpgdir). The kernel uses the
-// current process's page table during system calls and interrupts;
-// page protection bits prevent user code from using the kernel's
-// mappings.
-//
-// setupkvm() and exec() set up every page table like this:
-//
-//   0..KERNBASE: user memory (text+data+stack+heap), mapped to
-//                phys memory allocated by the kernel
-//   KERNBASE..KERNBASE+EXTMEM: mapped to 0..EXTMEM (for I/O space)
-//   KERNBASE+EXTMEM..data: mapped to EXTMEM..V2P(data)
-//                for the kernel's instructions and r/o data
-//   data..KERNBASE+PHYSTOP: mapped to V2P(data)..PHYSTOP,
-//                                  rw data + free physical memory
-//   0xfe000000..0: mapped direct (devices such as ioapic)
-//
-// The kernel allocates physical memory for its heap and for user memory
-// between V2P(end) and the end of physical memory (PHYSTOP)
-// (directly addressable from end..P2V(PHYSTOP)).
-
-// This table defines the kernel's mappings, which are present in
-// every process's page table.
-static struct kmap {
-	void* virt;
-	uintp phys_start;
-	uintp phys_end;
-	int perm;
-} kmap[] = {
-	{ (void*)KERNBASE, 0, EXTMEM, PTE_W },        // I/O space
-	{ (void*)KERNLINK, V2P(KERNLINK), V2P(data), 0 }, // kern text+rodata
-	{ (void*)data, V2P(data), PHYSTOP, PTE_W },   // kern data+memory
-	{ (void*)DEVBASE, DEVSPACE, 0, PTE_W },       // more devices
-};
-
-// Set up kernel part of a page table.
-pde_t* setupkvm(void){
-	pde_t* pgdir;
-	struct kmap* k;
-
-	if ((pgdir = (pde_t*)kalloc()) == 0)
-		return 0;
-	memset(pgdir, 0, PGSIZE);
-	if (p2v(PHYSTOP) > (void*)DEVSPACE)
-		panic("PHYSTOP too high");
-	for (k = kmap; k < &kmap[NELEM(kmap)]; k++)
-		if (mappages(pgdir, k->virt, k->phys_end - k->phys_start,
-		             (uint)k->phys_start, k->perm) < 0)
-			return 0;
-	return pgdir;
-}
-
-// Allocate one page table for the machine for the kernel address
-// space for scheduler processes.
-void kvmalloc(void){
-	kpgdir = setupkvm();
-	switchkvm();
-}
-
-// Switch h/w page table register to the kernel-only page table,
-// for when no process is running.
-void switchkvm(void){
-	lcr3(v2p(kpgdir)); // switch to the kernel page table
-}
-
-// Switch TSS and h/w page table to correspond to process p.
-void switchuvm(struct proc* p){
-	pushcli();
-	cpu->gdt[SEG_TSS] = SEG16(STS_T32A, &cpu->ts, sizeof(cpu->ts) - 1, 0);
-	cpu->gdt[SEG_TSS].s = 0;
-	cpu->ts.ss0 = SEG_KDATA << 3;
-	cpu->ts.esp0 = (uintp)proc->kstack + KSTACKSIZE;
-	ltr(SEG_TSS << 3);
-	if (p->pgdir == 0)
-		panic("switchuvm: no pgdir");
-	lcr3(v2p(p->pgdir)); // switch to new address space
-	popcli();
-}
-
-#endif
 
 // Load the initcode into address 0 of pgdir.
 // sz must be less than a page.
@@ -350,10 +240,3 @@ int copyout(pde_t* pgdir, uint va, void* p, uint len){
 	}
 	return 0;
 }
-
-
-// Blank page.
-
-// Blank page.
-
-// Blank page.
