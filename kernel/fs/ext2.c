3f451d0846c6c0c5c57e96f2377fcc149f59d514
Jason Whitehorn <jason@whitehorn.us>
2021-12-06 14:38:11 -0600

    abstract disk reads to not assume fixed block size

--- kernel/fs/ext2.c
+++ kernel/fs/ext2.c
@@ -7,27 +7,31 @@
 
 
 struct ext2_superblock sb;
+#define DISK_SECTOR_SIZE 512
+
+static inline void readblock(uint16 devt, uint32 devnum, uint64 blocknum, uint32 blocksize, void *buf) {
+    uint32 spb = blocksize / DISK_SECTOR_SIZE; // 2
+    uint64 start = blocknum * spb;
+    uint32 legacydevid = TODEVNUM(devt, devnum);
+
+    for(uint8 i = 0; i != spb; i++) {
+        struct buf* bp = bread(legacydevid, start + i);
+        void *offset = buf + (DISK_SECTOR_SIZE * i);
+        memcopy(offset, &bp->data[0], DISK_SECTOR_SIZE);
+        brelse(bp);
+    }
+}
 
 uint8 ext2_init_dev(uint16 devt, uint32 devnum) {
     ext2_readsb(devt, devnum, &sb);
-
     return (sb.ext2_signature == FS_EXT2_SIGNATURE) ? 1 : 0;
 }
 
 void ext2_readsb(uint16 devt, uint32 devnum, struct ext2_superblock* sb2) {
-    // assumes 512 byte sectors...
-    struct buf* bp1 = bread(TODEVNUM(devt, devnum), 2);
-    struct buf* bp2 = bread(TODEVNUM(devt, devnum), 3);
-
-    memcopy(&sb, &bp1->data[0], 512);
-    void *offset = &sb;
-    offset += 512;
-    memcopy(offset, &bp2->data[0], 512);
-
-    brelse(bp1);
-    brelse(bp2);
-
-    memcopy(sb2, &sb, sizeof(sb));
+    readblock(devt, devnum, 1, 1024, &sb);
+    if (&sb != sb2) {
+        memcopy(sb2, &sb, sizeof(sb));
+    }
 }
 
 int ext2_readi(struct inode *ip, char *dst, uint off, uint n) {
